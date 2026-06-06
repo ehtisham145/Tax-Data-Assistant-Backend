4bbc83e26d9ad81e03c4a1021afad16ac89484ba
ehtisham145 <ehtisham2406@gmail.com>
2026-06-06 23:36:12 +0500

    update

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="E-Numerak Tax Chatbot API")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://e-numerak.com","http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
