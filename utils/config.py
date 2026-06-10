b15cb82123488690c808e7c7f043910b58b417a0
Ehtisham <ehtisham@example.com>
2026-06-10 02:16:04 -0700

    Updated DB According to railway volume

--- utils/config.py
+++ utils/config.py
@@ -4,13 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_MODEL = "gpt-4o-mini"
 JINA_API_KEY = os.getenv("JINA_API_KEY")
 GROQ_MODEL = "llama-3.1-8b-instant"
-CHROMA_DB_PATH = "./chroma_db"
-SQLITE_DB_PATH = "users.db"
+CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "/data/chroma_db")  # ✅ Railway Volume
+SQLITE_DB_PATH = os.getenv("SQLITE_DB_PATH", "/data/users.db")   # ✅ Railway Volume
 MAX_HISTORY = 6
 SIMILARITY_TOP_K = 3
 REDIS_URL = "redis://localhost:6379"
-ALLOWED_ORIGINS = ["https://e-numerak.com","http://localhost:3000","https://tax-chatbot-front-end-production.up.railway.app"]
+ALLOWED_ORIGINS = [
+    "https://e-numerak.com",
+    "http://localhost:3000",
+    "https://tax-chatbot-front-end-production.up.railway.app"
+]
